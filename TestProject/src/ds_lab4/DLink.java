ca9b6a96ea3b8b25eb167fc39f90f33562244680
jvu02 <jvu02@DESKTOP-7KJCC92>
2021-04-01 10:39:25 -0400

    add some comments

--- TestProject/src/ds_lab4/DLink.java
+++ TestProject/src/ds_lab4/DLink.java
@@ -1,16 +1,16 @@
 package ds_lab4;
 
-public class DLink<T> {
+public class DLink<T> { //double link list
 	public T value;
 	public T[] data;
 	public DLink next, prev;
-	DLink(T value) {
+	DLink(T value) { //constructor for a single value
 		this.value =value;//1
 		next = null; //1
 		prev = null; //1
 	}
 	
-	DLink(T[] array){
+	DLink(T[] array){ //constructor for an array/head link
 		this.data=array;
 		next=null;
 		prev=null;
